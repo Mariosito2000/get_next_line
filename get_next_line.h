b486953e4bd91568c605f09c4fa7b533bf4df454
marias-e <marias-e@student.42madrid.com>
2022-10-06 12:05:54 +0200

    primerito try

---
+++ get_next_line.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marias-e <marias-e@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/03 13:03:17 by marias-e          #+#    #+#             */
+/*   Updated: 2022/10/04 17:06:43 by marias-e         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <string.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <fcntl.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+
+char		*get_next_line(int fd);
+size_t		ft_strlen(const char *s);
+char		*ft_strdup(const char *str);
+char		*ft_strjoin(char const *s1, char const *s2);
+int			ft_strchr(const char *s, int c);
+char		*ft_substr(char const *s, unsigned int start, size_t len);
+
+#endif
